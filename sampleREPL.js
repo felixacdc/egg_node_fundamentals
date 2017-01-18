7376d2c6256b19a1a8f8e655c661261fee99d2f5
felixacdc <pfmata01@gmail.com>
2017-01-17 22:22:48 -0600

    [add] using the nodejs repl shell :bear:

---
+++ sampleREPL.js
@@ -0,0 +1,20 @@
+1+2
+4*5
+var foo = "name"
+var boo = "other name"
+foo
+boo
+1+2+3
+_+4
+var myname = function() { return "will" }
+myname()
+var sayMyName = function() {
+return 'walter'
+}
+sayMyName()
+var mistake = function() {
+
+var mistake = function() {
+
+var mistake = function() {
+var mistake = function() {
