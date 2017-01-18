a9bae5b6445f6600c27d9fc6c28736e3ce83063f
felixacdc <pfmata01@gmail.com>
2017-01-17 22:45:44 -0600

    [modify] understanding callbacks in node js

--- two.js
+++ two.js
@@ -10,3 +10,14 @@ var look = function(dir) {
 if(dir === 'west') { console.log("There is a small mailbox here."); }
 if(dir === 'east') { console.log("You are standing next a white house."); }
 }
+var walk = function(dir) {
+if(dir === 'west') { console.log("It is very dark, you are likely to be eaten by a grue!"); }
+if(dir === 'south') { console.log("You are standing on the edge of a deep chasm."); }
+}
+var getInput = function(param, cb) {
+cb(param);
+}
+getInput('west', look)
+getInput('east', look)
+getInput('west', walk)
+getInput('south', walk)
