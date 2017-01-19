48e81fa746a9d2e4d48375df7b099e6a745e20b4
felixacdc <pfmata01@gmail.com>
2017-01-18 23:02:06 -0600

    [modify] buffers

--- five.js
+++ five.js
@@ -9,3 +9,7 @@ console.log(data);
 })
 str = '\u0048\u0065\u006c\u006c\u006f \u0057\u006f\u0072\u006c\u0064\u0021';
 str.length
+Buffer.byteLength(str, 'utf8')
+str = '\u00bd + \u00bc = \u00be';
+str.length
+Buffer.byteLength(str, 'utf8')
