ecedda256cb1f62031d1c032bd61361b162b1739
felixacdc <pfmata01@gmail.com>
2017-01-19 14:33:16 -0600

    [add] introduction to modules

---
+++ six.js
@@ -0,0 +1,6 @@
+var circle = require('./circle');
+circle.area(2)
+circle.circumference(4)
+var http = require('http');
+var colors = require('colors')
+console.log('All the colors of the rainbow'.rainbow);
